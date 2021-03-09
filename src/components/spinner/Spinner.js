b6b3449ccc6af5220b0dc6cec716d9dd62d23676
stevenacz <scoailazaa@gmail.com>
2021-03-09 11:50:55 -0500

    spinner component added

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,15 @@
+// React
+import React from 'react';
+
+// Styles
+import { StyledSpinner } from './Styles';
+
+const Spinner = () => {
+  return (
+    <StyledSpinner>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    </StyledSpinner>
+  )
+}
+
+export default Spinner
