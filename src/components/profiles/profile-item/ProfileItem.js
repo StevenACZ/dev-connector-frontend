d6d72a579c71f5bc90a57a05fa62c67f39488f3e
stevenacz <scoailazaa@gmail.com>
2021-03-11 14:27:39 -0500

    Profile item added

---
+++ src/components/profiles/profile-item/ProfileItem.js
@@ -0,0 +1,48 @@
+// React
+import React from 'react';
+
+// React Router
+import { Link } from 'react-router-dom';
+
+const ProfileItem = ({
+  user: {
+    _id,
+    name,
+    avatar
+  },
+  status,
+  company,
+  location,
+  skills
+}) => {
+  return (
+    <div className="profile bg-light">
+      <img src={ avatar } alt="" className="round-img"/>
+      <div>
+        <h2>{ name }</h2>
+        <p>{ status } { company && <span> at { company }</span>} </p>
+        <p className="my-1">{ location && <span>{ location }</span> }</p>
+        <Link
+          to={`/profile/${_id}`}
+          className="btn btn-primary"
+        >
+          View Profile
+        </Link>
+      </div>
+      <ul>
+        {
+          skills.slice(0, 4).map( ( skill, index ) => (
+            <li
+              key={ index }
+              className="text-primary"
+            >
+              { skill }
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default ProfileItem
