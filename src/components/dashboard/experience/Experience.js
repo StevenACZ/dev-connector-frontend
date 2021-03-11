f9f7a2ca873b3acda720d78d943d21626046927f
stevenacz <scoailazaa@gmail.com>
2021-03-10 21:13:24 -0500

    experience web page added

---
+++ src/components/dashboard/experience/Experience.js
@@ -0,0 +1,57 @@
+// React
+import React from 'react';
+
+// Redux
+// import { useDispatch } from 'react-redux';
+
+// Redux - Reducers
+
+// Moment
+import Moment from 'react-moment';
+
+const Experience = ({ experience }) => {
+  // const dispatch = useDispatch();
+  // const history =  useHistory();
+
+  const experiences = experience.map( exp => (
+    <tr key={ exp._id }>
+      <td>{ exp.company }</td>
+      <td className="hide-sm">{ exp.title }</td>
+      <td>
+        <Moment format='YYYY/MM/DD'>{ exp.from }</Moment> - {
+          exp.to === null ? (
+            'Now'
+          ) : (
+            <Moment format='YYYY/MM/DD'>{ exp.to }</Moment>
+          )
+        }
+      </td>
+      <td>
+        <button className="btn btn-danger">Delete</button>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <>
+      <h2 className="my-2">Experience Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th className="hide-sm">Title</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {
+            experiences
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Experience
